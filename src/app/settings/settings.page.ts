9bbc9245a15c1fbd7072e8fda382a5fb67484003
Faizaan Pervaiz <fp361@cam.ac.uk>
2019-11-03 18:10:22 +0000

    Settings storage

--- src/app/settings/settings.page.ts
+++ src/app/settings/settings.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FcmService } from './../services/fcm.service';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'app-settings',
@@ -7,9 +9,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingsPage implements OnInit {
 
-  constructor() { }
+  global_notifs: boolean
+  level_notifs: string
+
+  globalDisabled: string
+
+  constructor(private storage: Storage,
+    private fcmservice: FcmService) { }
 
   ngOnInit() {
+    this.getSettings();
+  }
+
+  async getSettings() {
+    this.global_notifs = await this.storage.get('global_notifs');
+    this.level_notifs = await this.storage.get('level_notifs');
+    this.toggleDisable();
+  }
+
+  settingsChanged() {
+    this.storage.set('global_notifs', this.global_notifs);
+    this.storage.set('level_notifs', this.level_notifs);
+    this.toggleDisable();
+    this.updateFcm();
+  }
+
+  toggleDisable() {
+    if (!this.global_notifs) {
+      this.globalDisabled = 'true';
+    }
+    else {
+      this.globalDisabled = 'false';
+    }
+  }
+
+  updateFcm() {
+    if (this.globalDisabled) {
+      // Disable all notifications
+      this.fcmservice.topicUnsubscribe('all');
+    }
+    else {
+      if (this.level_notifs == 'all') {
+        this.fcmservice.topicSubscribe('all');
+      }
+      else {
+        this.fcmservice.topicUnsubscribe('all');
+      }
+    }
   }
 
 }
